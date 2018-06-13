a12a0e924e78871863a6fabb2b078cb96d7958a9
bijeesraj <bijeesraj007@gmx.com>
2018-06-13 07:22:26 -0500

    Initial

---
+++ src/main/java/com/us/itp/odl/OdlAppApplication.java
@@ -0,0 +1,12 @@
+package com.us.itp.odl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdlAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdlAppApplication.class, args);
+    }
+}
